67ab5262fc691a64faeb9699e0f48e095b9b0bd0
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2017-02-01 22:59:20 +0000

    handle request when the passed param is a timestamp

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 var express = require('express')
-var app = express()
+var app = express();
 
+// handle request for the home page
 app.get('/', function (req, res) {
-  res.send('Hello big World !')
+  res.send('enter a timestamp in the url !');
 })
 
+// handle request when passing a param 
+app.get("/:date", function(req, res) {
+    
+    // variable to store the response
+    var jsonRes = {
+        unix: null,
+        natural: null
+    };
+    
+    // check if the passed param is a valid timestamp
+    if( !Number.isNaN( Number(req.params.date) ) ) {
+        
+        jsonRes.unix = Number(req.params.date);
+        
+        // convert the timestamp to a date obj
+        var dateObj = new Date(Number(req.params.date) * 1000);
+        
+        var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+            
+        var dateString = monthNames[dateObj.getUTCMonth()] + 
+            " " + dateObj.getUTCDate() + 
+            ", " + dateObj.getUTCFullYear();
+            
+        jsonRes.natural = dateString;
+    }
+    res.send(jsonRes);
+})
+
+
 app.listen(process.env.PORT, process.env.IP, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Example app listening on port 8080!');
 })
