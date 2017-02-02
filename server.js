69f8ce20776a9e3862363695f9dcf3fc119bf7d8
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2017-02-02 16:37:02 +0000

    some code optimization

--- server.js
+++ server.js
@@ -18,22 +18,22 @@ app.get("/:date", function(req, res) {
     // check if the passed param is a valid timestamp
     if( !Number.isNaN( Number(req.params.date) ) ) {
         
-        jsonRes.unix = Number(req.params.date);
-        
         // convert the timestamp to a date obj
         var dateObj = new Date(Number(req.params.date) * 1000);
         
         var monthNames = ["January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"];
-            
-        var dateString = monthNames[dateObj.getUTCMonth()] + 
+        
+        // assign the timestamp to unix property
+        jsonRes.unix = Number(req.params.date);
+        
+        //assign the natural date to natural property
+        jsonRes.natural = monthNames[dateObj.getUTCMonth()] + 
             " " + dateObj.getUTCDate() + 
             ", " + dateObj.getUTCFullYear();
-            
-        jsonRes.natural = dateString;
         
     // check if the passed param is a natural language date
-    } else if (Date.parse(req.params.date)) {
+    } else if ( !Number.isNaN( Date.parse(req.params.date) ) ) {
         
         // assign the timestamp to unix property
         jsonRes.unix = Date.parse(req.params.date) / 1000;
@@ -45,7 +45,6 @@ app.get("/:date", function(req, res) {
     // return the response in JSON format
     res.json(jsonRes);
 })
-
 
 app.listen(process.env.PORT, process.env.IP, function () {
   console.log('Example app listening on port 8080!');
